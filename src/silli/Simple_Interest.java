764e7e8dc35b2238cf67048652a0d94090b7e7b2
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-06-11 10:53:00 +0530

    programs

--- src/silli/Simple_Interest.java
+++ src/silli/Simple_Interest.java
@@ -1,4 +1,16 @@
 package silli;
 
+import java.util.Scanner;
+
 public class Simple_Interest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double principle , interest , time ;
+
+        principle= s.nextFloat();
+        interest= s.nextFloat();
+        time= s.nextFloat();
+        double sP = (( principle*interest)*time)/100;
+        System.out.println(sP);
+    }
 }
---
+++ src/silli/evenodd.java
@@ -0,0 +1,4 @@
+package silli;
+
+public class evenodd {
+}
