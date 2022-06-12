70e8ee7532d9bd3e7e4162495b169864ba4ace4c
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-06-12 09:20:50 +0530

    simple programs

---
+++ src/silli/N_NaturalNumvers.java
@@ -0,0 +1,15 @@
+package silli;
+
+import java.util.Scanner;
+
+public class N_NaturalNumvers {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
--- src/silli/evenodd.java
+++ src/silli/evenodd.java
@@ -1,4 +1,15 @@
 package silli;
 
+import java.util.Scanner;
+
 public class evenodd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner( System.in);
+        int a = s.nextInt();
+        if ( a %2 == 0 ){
+            System.out.println("this is EVEN number ");
+        }else{
+            System.out.println("this is odd number ");
+        }
+    }
 }
